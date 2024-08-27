0d61f0b95ea844a2b0f5bbd5c5f59bf6b7478708
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-08-27 11:50:15 +0530

    feat:frontend api changed

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,33 +1,32 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./Home.module.css";
 import { getDate } from "../../services/functions";
 import LogoutButton from "../../components/Logout/Logout";
 import Loader from "../../components/Loader/Loader";
 function App() {
   const [articles, setArticles] = useState(null);
-  const [query, setQuery] = useState("all");
+  const [query, setQuery] = useState("international");
   const deImage =
     "https://www.euractiv.com/wp-content/uploads/sites/2/2014/03/news-default.jpeg";
-  useEffect(() => {
-    const fetchNews = async () => {
-      const url = `https://newsapi.org/v2/everything?q=${query}&language=en&from=${getDate()}&sortBy=popularity&apiKey=37b720850b0e4450a50a8b543e831d94`;
-      // const url = `https://newsdata.io/api/1/news?apikey=pub_506169d1fc95f40f1ea7d9b69472ceeae2f2d&q=india  `;
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        const sortedArticles = data.articles.sort(
-          (a, b) => new Date(b.publishedAt) - new Date(a.publishedAt)
-        );
-        setArticles(sortedArticles.slice(0, 21));
-        console.log("news readr");
-      } catch (error) {
-        console.error("Error fetching the news:", error);
-      }
-    };
-
-    fetchNews();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query]);
+    useEffect(() => {
+      const fetchNews = async () => {
+        const url = `https://news-service-api.vercel.app/news?date=${getDate()}&category=${query}`;
+        try {
+          const response = await axios.get(url);
+          const sortedArticles = response.data.data.sort(
+            (a, b) => new Date(b.publishedAt) - new Date(a.publishedAt)
+          );
+          setArticles(sortedArticles.slice(0, 21));
+        } catch (error) {
+          console.error("Error fetching the news:", error);
+          axios.post("https://news-service-api.vercel.app/news/save")
+        }
+      };
+    
+      fetchNews();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [query]);
   if (!articles) {
     return <Loader />;
   }
@@ -45,7 +44,7 @@ function App() {
               <li>
                 <p
                   onClick={() => {
-                    setQuery("all");
+                    setQuery("international");
                   }}
                 >
                   Global
@@ -54,7 +53,7 @@ function App() {
               <li>
                 <p
                   onClick={() => {
-                    setQuery("India");
+                    setQuery("india");
                   }}
                 >
                   National
@@ -70,8 +69,12 @@ function App() {
                 </p>
               </li>
               <li>
+                <button onClick={()=>{axios.post("https://news-service-api.vercel.app/news/save")}}>Update news</button>
+              </li>
+              <li>
                 <LogoutButton />
               </li>
+
             </ul>
           </nav>
         </div>
--- Frontend/src/services/functions.js
+++ Frontend/src/services/functions.js
@@ -2,9 +2,9 @@ const getDate=()=>{
     const today = new Date();
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
+    const day = String(today.getDate())
     
-    return `${year}-${month}-${day-2}`;
+    return `${year}-${month}-${day}`;
       }
 
 export {getDate}
